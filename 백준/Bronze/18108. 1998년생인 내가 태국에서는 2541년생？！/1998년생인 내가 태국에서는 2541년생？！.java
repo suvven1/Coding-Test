9ca0d8cbfe1a357095b2b0a61671d091e1166a5b
Jinsu Choi <135982883+suvven1@users.noreply.github.com>
2023-12-23 17:33:36 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 108 ms, Memory: 12792 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String years = scanner.nextLine();
+        System.out.println(Integer.parseInt(years) - 543);
+    }
+}
