7ed3d8c0eb88172a814fb4e29d781f70231b52aa
Jinsu Choi <135982883+suvven1@users.noreply.github.com>
2023-12-23 17:33:04 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 12792 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String dupName = scanner.nextLine();
+        System.out.println(dupName + "??!");
+    }
+}
