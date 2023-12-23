01fa1f148ee0ac19cd64c25df6dc5aaa9d460fd7
Jinsu Choi <135982883+suvven1@users.noreply.github.com>
2023-12-23 21:52:46 +0900

    [Bronze III] Title: 알람 시계, Time: 108 ms, Memory: 12848 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static int hour;
+    public static int minute;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] hourAndMinute = sc.nextLine().split(" ");
-        int hour = Integer.parseInt(hourAndMinute[0]);
-        int minute = Integer.parseInt(hourAndMinute[1]);
+        hour = Integer.parseInt(hourAndMinute[0]);
+        minute = Integer.parseInt(hourAndMinute[1]);
+        getMinute();
+        getHour();
+        System.out.println(hour + " " + minute);
+    }
 
+    public static void getHour(){
+        if(hour == -1){
+            hour = 23;
+        }
+    }
+
+    public static void getMinute(){
         if(minute >= 45){
             minute = minute - 45;
-        }else{
-            if(hour == 0){
-                hour = 23;
-            }else {
-                hour = hour - 1;
-            }
-            minute = 60 + (minute - 45);
+            return;
         }
-
-        System.out.println(hour + " " + minute);
+        hour = hour - 1;
+        minute = 60 + (minute - 45);
     }
 }
