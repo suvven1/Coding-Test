162fc6a401c7d50d0c292bf769d7dcd2b41b2de1
Jinsu Choi <135982883+suvven1@users.noreply.github.com>
2023-12-23 17:31:04 +0900

    [Bronze V] Title: A-B, Time: 104 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.nextLine();
+        String[] intNum = num.split(" ");
+        System.out.println(Integer.parseInt(intNum[0]) - Integer.parseInt(intNum[1]));
+    }
+}
