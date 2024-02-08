d6a55f9268656f9d72d2e7e0f681b5e8c113cda1
Jinsu Choi <135982883+suvven1@users.noreply.github.com>
2024-02-08 20:37:39 +0900

    [Bronze V] Title: 오늘 날짜, Time: 116 ms, Memory: 13064 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+    }
+}
