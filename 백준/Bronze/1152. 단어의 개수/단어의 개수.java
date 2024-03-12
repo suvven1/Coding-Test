09694107fe5cae743c2fbce6ad3a7f4bff80db62
Jinsu Choi <135982883+suvven1@users.noreply.github.com>
2024-03-12 14:28:10 +0900

    [Bronze II] Title: 단어의 개수, Time: 168 ms, Memory: 19512 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,16 +1,11 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = reader.readLine().split(" ");
-        
-        if(input.length == 0){
-            System.out.println(input.length);
-        }else if(input[0].isEmpty()){
-            System.out.println(input.length -1);
-        }else{
-            System.out.println(input.length);
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " "); // 빈값은 삭제
+
+        System.out.print(st.countTokens());
     }
 }
