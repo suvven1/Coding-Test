480690e026d547b8de9c2051596bcb181e903e8c
Jinsu Choi <135982883+suvven1@users.noreply.github.com>
2024-10-31 19:46:48 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.84 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -3,32 +3,10 @@ import java.util.HashMap;
 
 class Solution {
     public int solution(String s) {
-        String answer ="";
-        String str = "";
-        char[] charArray = s.toCharArray();
         String[] enNum = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-        Map<String, String> numMap = new HashMap<>();
-        for (int i=0; i<10; i++) {
-            numMap.put(enNum[i], String.valueOf(i));
+        for(int i=0; i<enNum.length; i++){
+            s = s.replaceAll(enNum[i], String.valueOf(i));
         }
-        
-        for(char c : charArray){
-            String strC = String.valueOf(c);
-
-            try {
-                int i = Integer.parseInt(strC);
-                answer += strC;
-            } catch (NumberFormatException e) {
-                str += strC;
-                String ans = numMap.get(str);
-                if(ans != null){
-                    answer += ans;
-                    str = "";
-                }
-                continue;
-            }
-        }
-        
-        return Integer.parseInt(answer);
+        return Integer.parseInt(s);
     }
 }
